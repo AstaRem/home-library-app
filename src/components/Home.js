fae44108be96dcf11bd067ac42f7f3e8a7893c21
EB <e.bains@hotmail.co.uk>
2023-06-01 21:48:00 +0100

    Added code to componet js and css files.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import '../css/Home.css';
+
 
 function Home() {
   return (
     <div>
-      
+      <div className="jumbotron">
+      <div className="container text-center" id="search-button">
+
+<form className="form-center">
+  <input className="form-control" type="text" placeholder="Search" aria-label="Search"/>
+  <br></br>
+  <button className="btn btn-primary btn-lg" type="submit">SEARCH</button>
+</form>
+
+
+</div>
+
+
+
+
+
+      </div>
     </div>
   )
 }
