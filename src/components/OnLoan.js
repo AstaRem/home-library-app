96799bc96b7770ea1759fb03be10298377ba1a4f
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2023-06-01 13:56:54 +0100

    added OnLoanModal and bootstrap script tags in public/index.html

--- src/components/OnLoan.js
+++ src/components/OnLoan.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 const OnLoan = () => {
   return (
-    <div>OnLoan</div>
-  )
+//     <div>
+// <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+// </button>
+
+// <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+//   <div className="modal-dialog">
+//     <div className="modal-content">
+//       <div className="modal-header">
+//         <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+//         <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+//           <span aria-hidden="true">&times;</span>
+//         </button>
+//       </div>
+//       <div className="modal-body">
+        
+//       </div>
+//       <div className="modal-footer">
+//         <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+//         <button type="button" className="btn btn-primary">Save changes</button>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+//     </div>
+<div>
+  <h1>OnLoan</h1>
+</div>
+    )
 }
 
-export default OnLoan
+export default OnLoan;
