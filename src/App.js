fe7172c8f32218ab5b4217fb1011bbccb89cbc38
AdrianWong <adrianwg04@gmail.com>
2023-06-01 10:23:17 +0100

    commit changes of App.js before merge

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
-import BooksOnLoan from './components/BooksOnLoan';
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
-    <BooksOnLoan>
-   {/* <div className="App">
+   <div className="App">
       <header className="App-header">
       Here will be our Home Library App
       </header>
-    </div> */}
-    </BooksOnLoan>
+    </div>
   );
 }
 
