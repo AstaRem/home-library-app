5f3e6d9259fdfcd70db9520c61af837ef8c9207d
AdrianWong <adrianwg04@gmail.com>
2023-06-01 21:24:38 +0100

    Modified App.js, BooksOnLoan, Header and Home components,added BookCard component and data.

---
+++ src/components/BookCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function BookCard() {
+  return (
+    <div className="card book_card">
+        <img src="https://books.google.com/books/content?id=LdQtzwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api" className="card-img-top" alt="..." />
+        <div className="card-body">
+            <p>hfjdjdffjhdhjdf</p>
+            {/* <h5 className="card-title">Card title</h5> */}
+            {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+
+            {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+        </div>
+</div>
+  )
+}
+
+export default BookCard
