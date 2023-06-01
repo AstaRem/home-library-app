a6616f090d4b3a6488301b325fc71c30ee25b910
AdrianWong <adrianwg04@gmail.com>
2023-06-01 10:04:48 +0100

    Modified BooksOnLoan.js and css, with using the mock data to display the borrow booklist successfully.Modified index.css to change the hover color of the btn-default class.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import BooksOnLoan from './components/BooksOnLoan';
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
-    <div className="App">
+    <BooksOnLoan>
+   {/* <div className="App">
       <header className="App-header">
       Here will be our Home Library App
       </header>
-    </div>
+    </div> */}
+    </BooksOnLoan>
   );
 }
 
