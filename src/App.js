c3140613653e1fc622ca4c39b3d960544dac1a9d
AdrianWong <adrianwg04@gmail.com>
2023-06-02 20:08:02 +0100

    Added the functionality of the Add New Book in Home component.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import BooksOnLoan from './components/BooksOnLoan';
+//import AddNewBook from './components/AddNewBook';
 import 'bootstrap/dist/css/bootstrap.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEllipsis} from '@fortawesome/free-solid-svg-icons';
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="BooksOnLoan" element={<BooksOnLoan />} />
+          {/* <Route path="AddNewBook" element={<AddNewBook />} /> */}
         </Routes>
       </div>
     </Router>
