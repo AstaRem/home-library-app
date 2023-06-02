338ea6b53b331ec18aee225e6027e83d2a57558f
milkibeka <125259404+milkibeka@users.noreply.github.com>
2023-06-02 13:44:22 +0100

    general template of add new book created in content wrapper

---
+++ src/components/AddNewBook.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ContentWrapper from './ContentWrapper';
+import '../css/AddNewBook.css';
+
+const AddNewBook = () => {
+
+
+  return (
+  <ContentWrapper pageTitle="Add New Book">
+      <div className="add-new-book">
+          
+      </div>
+  </ContentWrapper> 
+  )
+}
+
+export default AddNewBook
