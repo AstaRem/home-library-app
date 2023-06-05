efbd3d7612f5ec2f4cab67df2b3ae8704437d900
EB <e.bains@hotmail.co.uk>
2023-06-05 20:44:54 +0100

    Worked with Adrian to make search results display.

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,11 +12,16 @@ const Home = (props) => {
   const [bookList, setBookList] = useState(props.data);
   const [selectedItem, setSelectedItem] = useState(null);
   const [selectedBook_details, setSelectedBook_details] = useState(null);
+  const [query, setQuery]= useState ("");
 
   // keep track of bookList
   useEffect(() => {
     console.log("bookList:", bookList);
    }, [bookList]);
+
+   useEffect(() => {
+    console.log("query:", query);
+   }, [query]);
 
   
   //Function for opening on loan modal
@@ -39,14 +44,79 @@ const Home = (props) => {
       }
     }
 
+  //Function for searching Library
+
+
+  function searchLibrary(event){
+    event.preventDefault();
+    // const searchResults=[];
+
+    const searchResults = bookList.filter((book)=> book.title == query|| book.author == query|| book.ISBN == query);
+
+    // const splitQuery = query.split(" ");
+
+    // for (let i = 0; i < splitQuery.length; i++) {
+    //   const checkResults = bookList.filter((book)=> book.title == splitQuery[i]|| book.author == splitQuery[i]|| book.ISBN == splitQuery[i]);
+      
+    //   if(checkResults) {
+    //   // searchResults=[...searchResults,...checkResults];
+    
+    //   }
+    // }
+
+    // console.log("splitQuery",splitQuery);
+
+    console.log ("query" + query);
+    
+    console.log("searchResults: " + JSON.stringify(searchResults));
+
+    setBookList(searchResults);
+
+  }
+
+  function resetLibrary(){
+
+    setBookList(props.data);
+
+  }
+
+  function changeSearchInput(event){
+   
+     setQuery(event.target.value);
+     if (event.target.value=="") {
+      resetLibrary();
+
+     }
+
+  }
+
+  // const[searchbookList, setSearchbookList]
+  // const searchbookList = filter.bookList
+
+ 
+  
+
+//{bookList.filter}
+
+//}
+   // const searchLibrary=bookList.filter
+  //
+//const searchLibrary =(bookList) =>{}
+
+
+//setBookList(bookList);
+
+//const hand
+//onChange= {(e)=> setBooklist(e.target.value)}
+
 
   return (
     <div>
       <section className="jumbotron">
         <div className="container hero-banner text-center">
-          <form id="home_form_book_search">
+          <form id="home_form_book_search" onSubmit={searchLibrary} >
             <div className="row row_input">
-              <input className="form-control" type="text" placeholder="Please enter book title, author or ISBN" aria-label="Search" id="home_search_input"/>
+              <input className="form-control" type="text" placeholder="Please enter book title, author or ISBN" aria-label="Search" id="home_search_input" onChange= {(event)=> changeSearchInput(event) }/>
             </div>
             <button className="btn-default" type="submit" id="home_form_book_search_btn">SEARCH</button>
           </form>
@@ -89,6 +159,11 @@ const Home = (props) => {
         selectedBook={selectedBook_details}
         updateBookList = {updateBookList}
        />
+
+
+     {/* <searchLibrary
+       bookList={}
+       setbookList={} />  */}
     </div>
   )
 }
