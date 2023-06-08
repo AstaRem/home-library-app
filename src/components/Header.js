488feb173a6c6bf8454831be297aaf6d1522f622
AdrianWong <adrianwg04@gmail.com>
2023-06-08 17:46:02 +0100

    Tweak AddBook styling, changed logo.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../css/Header.css';
-import logo from '../assets/images/shelf_logo.png';
+import logo from '../assets/images/shelf_logo2.png';
 
 
 function Header() {
